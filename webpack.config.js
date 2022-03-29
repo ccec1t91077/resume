2240ebcc600db6a5556cf56b012835f0ba105326
林祺晟 <ccec1t91077@gmail.com>
2022-03-29 17:06:34 +0800

    Reveal example created.

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ttf|woff|eot)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: 'index.html' })
+    ]
+};
